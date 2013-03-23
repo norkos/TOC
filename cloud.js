cd1fdf8e27a25df81144e4ecf3f25941d87a3a71
Norbert Kosmal <norbert.kosmal@gmail.com>
2013-03-23 11:51:26 +0100

    Arrows

--- cloud.js
+++ cloud.js
@@ -7,13 +7,6 @@ var text = 30;
 var NPS = "I have problems in \nsolving conflicts", A= "Be known manager \nin my company", B= "Manage in the \nefficient way", C= "Fully understand\n the problem", D= "Take the fast decisions", DD= "Collect as much \ninformation as possible";
 var r;
 
-function resizePaper(){
-  var win = $(this);
-  if(r != null)
-  r.changeSize(win.width(), win.height(), true, false);
-}
-//$(window).resize(resizePaper); 
-
 function paint() {
 	$("#holder").html("");
 
@@ -55,12 +48,14 @@ function paint() {
 	var t = r.text(text + start, 1.5 * text + 280, A).attr({"text-anchor":"start",fill: '#ffffff',"font-size": 18});;
 
 	/* connections */
-	connections.push(r.connection(shapes[0], shapes[1], "#f00", "#f00|3")); // NPS B
-	connections.push(r.connection(shapes[1], shapes[2], "#808080 ")); // B D
-	connections.push(r.connection(shapes[3], shapes[4], "#808080 ")); // C DD
-	connections.push(r.connection(shapes[1], shapes[5], "#808080 ")); // B A
-	connections.push(r.connection(shapes[3], shapes[5], "#808080 ")); // A C ...
-	connections.push(r.connection(shapes[2], shapes[4], "#f00", "#f00|5")); // D DD
+	connections.push(r.connection(shapes[2], shapes[4], "#f00")); // D DD
+	connections.push(r.connection(shapes[4], shapes[2], "#f00")); // D DD
+	connections.push(r.connection(shapes[0], shapes[1], "#f00")); // NPS B
+
+	connections.push(r.connection2(shapes[2], shapes[1], "#D0D0D0")); // B D
+	connections.push(r.connection2(shapes[4], shapes[3], "#D0D0D0")); // C DD
+	connections.push(r.connection2(shapes[1], shapes[5], "#D0D0D0")); // B A
+	connections.push(r.connection2(shapes[3], shapes[5], "#D0D0D0")); // A C ...
 }
 
 function process() {
@@ -168,12 +163,97 @@ Raphael.fn.connection = function(obj1, obj2, line, bg) {
 	} else {
 		var color = typeof line == "string" ? line : "#000";
 		return {
-			bg : bg && bg.split && this.path(path).attr( {
-				stroke : bg.split("|")[0],
-				fill : "none",
-				"stroke-width" : bg.split("|")[1] || 5
-			}),
+			
 			line : this.path(path).attr( {
+				stroke : color,
+				fill : "none",
+				"stroke-width" : 3,
+			}),
+			bg : bg && bg.split && this.path(path).attr( {
+				stroke : color,
+				fill : "none"
+			}),
+			from : obj1,
+			to : obj2
+		};
+	}
+};
+
+Raphael.fn.connection2 = function(obj1, obj2, line, bg) {
+	if (obj1.line && obj1.from && obj1.to) {
+		line = obj1;
+		obj1 = line.from;
+		obj2 = line.to;
+	}
+	var bb1 = obj1.getBBox(), bb2 = obj2.getBBox(), p = [ {
+		x : bb1.x + bb1.width / 2,
+		y : bb1.y - 1
+	}, {
+		x : bb1.x + bb1.width / 2,
+		y : bb1.y + bb1.height + 1
+	}, {
+		x : bb1.x - 1,
+		y : bb1.y + bb1.height / 2
+	}, {
+		x : bb1.x + bb1.width + 1,
+		y : bb1.y + bb1.height / 2
+	}, {
+		x : bb2.x + bb2.width / 2,
+		y : bb2.y - 1
+	}, {
+		x : bb2.x + bb2.width / 2,
+		y : bb2.y + bb2.height + 1
+	}, {
+		x : bb2.x - 1,
+		y : bb2.y + bb2.height / 2
+	}, {
+		x : bb2.x + bb2.width + 1,
+		y : bb2.y + bb2.height / 2
+	} ], d = {}, dis = [];
+	for ( var i = 0; i < 4; i++) {
+		for ( var j = 4; j < 8; j++) {
+			var dx = Math.abs(p[i].x - p[j].x), dy = Math.abs(p[i].y - p[j].y);
+			if ((i == j - 4)
+					|| (((i != 3 && j != 6) || p[i].x < p[j].x)
+							&& ((i != 2 && j != 7) || p[i].x > p[j].x)
+							&& ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
+				dis.push(dx + dy);
+				d[dis[dis.length - 1]] = [ i, j ];
+			}
+		}
+	}
+	if (dis.length == 0) {
+		var res = [ 0, 4 ];
+	} else {
+		res = d[Math.min.apply(Math, dis)];
+	}
+	var x1 = p[res[0]].x, y1 = p[res[0]].y, x4 = p[res[1]].x, y4 = p[res[1]].y;
+	dx = Math.max(Math.abs(x1 - x4) / 2, 10);
+	dy = Math.max(Math.abs(y1 - y4) / 2, 10);
+	var x2 = [ x1, x1, x1 - dx, x1 + dx ][res[0]].toFixed(3), y2 = [ y1 - dy,
+			y1 + dy, y1, y1 ][res[0]].toFixed(3), x3 = [ 0, 0, 0, 0, x4, x4,
+			x4 - dx, x4 + dx ][res[1]].toFixed(3), y3 = [ 0, 0, 0, 0, y1 + dy,
+			y1 - dy, y4, y4 ][res[1]].toFixed(3);
+	var path = [ "M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3,
+			x4.toFixed(3), y4.toFixed(3) ].join(",");
+	if (line && line.line) {
+		line.bg && line.bg.attr( {
+			path : path
+		});
+		line.line.attr( {
+			path : path
+		});
+	} else {
+		var color = typeof line == "string" ? line : "#000";
+		return {
+			
+			line : this.path(path).attr( {
+				stroke : color,
+				fill : "none",
+				"stroke-width" : 3,
+				"arrow-end": "classic-wide-long"
+			}),
+			bg : bg && bg.split && this.path(path).attr( {
 				stroke : color,
 				fill : "none"
 			}),
