ab8071e03cb6d3611e44555d7f74806bf8a1ac41
Norbert Kosmal <norbert.kosmal@gmail.com>
2013-04-01 17:32:49 +0200

    done

--- js/cloud.js
+++ js/cloud.js
@@ -99,7 +99,7 @@ function paint() {
     dy23 = dy23 + dyfontMargin;
     dy = dy + dyfontMargin;
     
-    var fontSize = (y+x) /20;
+    var fontSize = (y+x) /16;
     MAX = x/15;
     
     NPS = splitMe(NPS);
